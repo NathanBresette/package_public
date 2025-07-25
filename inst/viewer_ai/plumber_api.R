619020e944dd178e0a2a35104e52173f5989aaf5
Nathan Bresette <nathanbresette04@gmail.com>
2025-07-24 20:24:45 -0500

    fix: convert all context values to strings to avoid join errors

--- inst/viewer_ai/plumber_api.R
+++ inst/viewer_ai/plumber_api.R
@@ -36,7 +36,7 @@ function() {
       list("Console History: Error reading")
     })
     
-    # Get workspace objects as a simple dictionary
+    # Get workspace objects as a simple dictionary with string values only
     workspace_objects <- tryCatch({
       objects <- ls(envir = .GlobalEnv)
       if (length(objects) > 0) {
@@ -45,25 +45,25 @@ function() {
           tryCatch({
             val <- get(obj, envir = .GlobalEnv)
             
-            # Create simple object info
+            # Create simple object info with string values only
             obj_info <- list(
               type = if ("data.frame" %in% class(val)) "dataframe" else if (is.vector(val)) "vector" else if (is.list(val)) "list" else if (is.function(val)) "function" else "other",
               size = if (is.data.frame(val)) paste(nrow(val), "rows,", ncol(val), "cols") else as.character(length(val)),
               class = paste(class(val), collapse = ", ")
             )
             
-            # Add simple type-specific details
+            # Add simple type-specific details as strings only
             if (is.data.frame(val)) {
-              obj_info$columns <- as.list(colnames(val))
-              obj_info$data_types <- as.list(sapply(val, function(x) paste(class(x), collapse = ", ")))
+              obj_info$columns <- paste(colnames(val), collapse = ", ")
+              obj_info$data_types <- paste(sapply(val, function(x) paste(class(x), collapse = ", ")), collapse = ", ")
             } else if (is.vector(val) && length(val) > 0) {
               obj_info$vector_type <- class(val)[1]
-              obj_info$sample_values <- as.list(as.character(head(val, 5)))
+              obj_info$sample_values <- paste(as.character(head(val, 5)), collapse = ", ")
             } else if (is.list(val)) {
               obj_info$length <- as.character(length(val))
-              obj_info$names <- as.list(names(val))
+              obj_info$names <- paste(names(val), collapse = ", ")
             } else if (is.function(val)) {
-              obj_info$arguments <- as.list(names(formals(val)))
+              obj_info$arguments <- paste(names(formals(val)), collapse = ", ")
             }
             
             obj_dict[[obj]] <- obj_info
@@ -79,9 +79,9 @@ function() {
       list(error = paste("Error reading workspace objects:", e$message))
     })
     
-    # Get environment information as a simple dictionary
+    # Get environment information as a simple dictionary with string values
     environment_info <- tryCatch({
-      # Get loaded packages as a simple list
+      # Get loaded packages as a simple string
       loaded_packages <- tryCatch({
         pkgs <- names(sessionInfo()$otherPkgs)
         if (length(pkgs) > 0) {
@@ -91,12 +91,12 @@ function() {
               paste(pkg, "v", version)
             }, error = function(e) pkg)
           })
-          as.list(pkg_versions)
+          paste(pkg_versions, collapse = ", ")
         } else {
-          list()
+          "No packages loaded"
         }
       }, error = function(e) {
-        list(error = paste("Error reading packages:", e$message))
+        paste("Error reading packages:", e$message)
       })
       
       list(
@@ -109,7 +109,7 @@ function() {
       list(error = paste("Error reading environment info:", e$message))
     })
     
-    # Get custom functions as a simple list
+    # Get custom functions as a simple string
     custom_functions <- tryCatch({
       objects <- ls(envir = .GlobalEnv)
       funcs <- objects[sapply(objects, function(obj) {
@@ -118,12 +118,16 @@ function() {
           is.function(val)
         }, error = function(e) FALSE)
       })]
-      as.list(funcs)
+      if (length(funcs) > 0) {
+        paste(funcs, collapse = ", ")
+      } else {
+        "No custom functions"
+      }
     }, error = function(e) {
-      list(error = paste("Error reading functions:", e$message))
+      paste("Error reading functions:", e$message)
     })
     
-    # Get plot history as a simple list
+    # Get plot history as a simple string
     plot_history <- tryCatch({
       plot_objects <- ls(envir = .GlobalEnv)[sapply(ls(envir = .GlobalEnv), function(obj) {
         tryCatch({
@@ -131,34 +135,38 @@ function() {
           inherits(val, c("ggplot", "trellis", "plot"))
         }, error = function(e) FALSE)
       })]
-      as.list(plot_objects)
+      if (length(plot_objects) > 0) {
+        paste(plot_objects, collapse = ", ")
+      } else {
+        "No plot objects"
+      }
     }, error = function(e) {
-      list(error = paste("Error reading plot history:", e$message))
+      paste("Error reading plot history:", e$message)
     })
     
-    # Get error history as a simple list
+    # Get error history as a simple string
     error_history <- tryCatch({
-      errors <- list()
+      errors <- c()
       if (exists(".Last.error", envir = .GlobalEnv)) {
         last_error <- get(".Last.error", envir = .GlobalEnv)
-        errors$last_error <- paste(capture.output(print(last_error)), collapse = "\n")
+        errors <- c(errors, paste("Last Error:", paste(capture.output(print(last_error)), collapse = " ")))
       }
       if (exists("last.warning", envir = .GlobalEnv)) {
         last_warnings <- get("last.warning", envir = .GlobalEnv)
         if (length(last_warnings) > 0) {
-          errors$warnings <- paste(capture.output(print(last_warnings)), collapse = "\n")
+          errors <- c(errors, paste("Warnings:", paste(capture.output(print(last_warnings)), collapse = " ")))
         }
       }
       if (length(errors) == 0) {
-        list("No recent errors")
+        "No recent errors"
       } else {
-        errors
+        paste(errors, collapse = "; ")
       }
     }, error = function(e) {
-      list(error = paste("Error reading error history:", e$message))
+      paste("Error reading error history:", e$message)
     })
     
-    # Return context in the format expected by the backend
+    # Return context in the format expected by the backend with string values only
     list(
       document_content = as.character(document_content),
       console_history = console_history,
