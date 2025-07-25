bc77e772b1b92bd246faf6959a6bca18717ecb83
Nathan Bresette <nathanbresette04@gmail.com>
2025-07-24 20:02:42 -0500

    fix: simplify plumber_api.R to minimal working structure for Plumber v1.x+

--- inst/viewer_ai/plumber_api.R
+++ inst/viewer_ai/plumber_api.R
@@ -1,20 +1,11 @@
-# Plumber API for RStudio integration
-# This provides local endpoints for the HTML interface to interact with RStudio
-
-library(plumber)
-library(rstudioapi)
-
-#' @plumber
+#* @plumber
 function(pr) {
-  # Serve static index.html at /
   pr$handle("GET", "/", function(req, res) {
     static_dir <- system.file("public", package = "rstudioai")
     res$setHeader("Cache-Control", "no-cache")
     res$sendFile(file.path(static_dir, "index.html"))
   })
-  pr$mount("/static", plumber::pr_static(system.file("public", package = "rstudioai")))
-
-  # POST /insert_code
+  
   pr$handle("POST", "/insert_code", function(req, res) {
     code <- req$body$code
     tryCatch({
@@ -24,11 +15,10 @@ function(pr) {
       list(success = FALSE, message = paste("Error inserting code:", e$message))
     })
   })
-
-  # GET /health
+  
   pr$handle("GET", "/health", function(req, res) {
     list(status = "healthy", service = "rstudio-plumber-api")
   })
-
+  
   pr
 }
