0fada39d9cd201d4b8a7fb41e728146e9a2d1377
Nathan Bresette <nathanbresette04@gmail.com>
2025-07-24 19:01:37 -0500

    fix: add find_available_port to main R/config.R for dynamic port assignment

--- R/config.R
+++ R/config.R
@@ -1,1 +1,15 @@
 
+# Find an available port for the plumber server
+find_available_port <- function(start_port = 8888, max_attempts = 100) {
+  for (port in start_port:(start_port + max_attempts)) {
+    tryCatch({
+      # Try to create a connection to test if port is available
+      con <- socketConnection(host = "127.0.0.1", port = port, server = TRUE, blocking = FALSE)
+      close(con)
+      return(port)
+    }, error = function(e) {
+      # Port is in use, try next one
+    })
+  }
+  stop("No available ports found in range ", start_port, ":", start_port + max_attempts)
+}
